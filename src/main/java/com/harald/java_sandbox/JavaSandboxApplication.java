68f7d307edd5605a1a5b1b9935f2de68a58628da
harald.hiemstra <haraldhiemstra@Haralds-MacBook-Pro.local>
2023-11-19 18:36:26 +0700

    failing build commit

---
+++ src/main/java/com/harald/java_sandbox/JavaSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.harald.java_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harald/java_sandbox/JavaSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harald.java_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
