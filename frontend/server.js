8054048c5f0c436a91c67ac7c519fb2a8ff87385
harald.hiemstra <haraldhiemstra@Haralds-MacBook-Pro.local>
2023-12-04 19:27:11 +0700

    working cors without spring security

--- frontend/server.js
+++ frontend/server.js
@@ -5,25 +5,19 @@ const cors = require("cors");
 const app = express();
 const port = 5500; // Choose any port number you prefer
 
-app.use(cors()); // Enable CORS for all routes
+// app.use(cors()); // Enable CORS for all routes
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log(req);
-  console.log(res);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // Route to serve the index.html file
 app.get("/", (req, res) => {
