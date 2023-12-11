5c5acf097c6e29394b91589c9ab49a22b5627dd8
harald.hiemstra <haraldhiemstra@Haralds-MBP.corp.capgemini.com>
2023-12-11 10:24:18 +0100

    fix cors

--- cors-test-java/src/main/java/com/harald/corstestjava/config/SecurityConfig.java
+++ cors-test-java/src/main/java/com/harald/corstestjava/config/SecurityConfig.java
@@ -1,85 +1,74 @@
-// package com.harald.corstestjava.config;
+package com.harald.corstestjava.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// // import org.springframework.security.config.Customizer;
-// // import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// // import org.springframework.security.core.userdetails.User;
-// // import org.springframework.security.core.userdetails.UserDetails;
-// // import
-// org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// // import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.CorsConfigurationSource;
-// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-// import java.util.Arrays;
-// import java.util.List;
-// //
-// @Configuration
-// public class SecurityConfig {
+@Configuration
+public class SecurityConfig {
 
-//     @Bean
-//     public InMemoryUserDetailsManager userDetailsManager() {
-//         UserDetails employee = User.builder().username("employee")
-//                 .password("{noop}root")
-//                 .roles("EMPLOYEE")
-//                 .build();
-//
-//         UserDetails manager = User.builder()
-//                 .username("manager")
-//                 .password("{noop}root")
-//                 .roles("EMPLOYEE", "MANAGER")
-//                 .build();
-//
-//         UserDetails admin = User.builder()
-//                 .username("admin")
-//                 .password("{noop}root")
-//                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
-//                 .build();
-//
-//         return new InMemoryUserDetailsManager(employee, manager, admin);
-//     }
-//
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws
-//     Exception {
-//         http.authorizeHttpRequests((configurer ->
-//                 configurer
-//                         .requestMatchers(HttpMethod.GET,
-//                         "/api/employees").hasRole("EMPLOYEE")
-//                         .requestMatchers(HttpMethod.GET,
-//                         "/api/employees/**").hasRole("EMPLOYEE")
-//                         .requestMatchers(HttpMethod.POST,
-//                         "/api/employees").hasRole("MANAGER")
-//                         .requestMatchers(HttpMethod.PUT,
-//                         "/api/employees").hasRole("MANAGER")
-//                         .requestMatchers(HttpMethod.DELETE,
-//                         "/api/employees").hasRole("ADMIN")
-//                         .requestMatchers(HttpMethod.GET,
-//                         "/basic/all").hasRole("EMPLOYEE")
-//                         .requestMatchers("/error").permitAll()
-//                         .anyRequest().permitAll()
-//
-//         ));
-//         http.httpBasic(Customizer.withDefaults());
-//
-//         http.csrf(csrf -> csrf.disable());
-//         return http.build();
-//     }
-//
-//     @Bean
-//     CorsConfigurationSource corsConfigurationSource() {
-//         CorsConfiguration config = new CorsConfiguration();
-//         config.setAllowedOrigins(Arrays.asList("*"));
-//         config.setAllowedMethods(Arrays.asList("GET"));
-//         config.setAllowedHeaders(List.of("Authorization"));
-//         UrlBasedCorsConfigurationSource source = new
-//         UrlBasedCorsConfigurationSource();
-//         source.registerCorsConfiguration("/**", config);
-//         return source;
-//     }
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    UserDetails employee = User.builder()
+                               .username("employee")
+                               .password("{noop}root")
+                               .roles("EMPLOYEE")
+                               .build();
 
-// }
+    UserDetails manager = User.builder()
+                              .username("manager")
+                              .password("{noop}root")
+                              .roles("EMPLOYEE", "MANAGER")
+                              .build();
+
+    UserDetails admin = User.builder()
+                            .username("admin")
+                            .password("{noop}root")
+                            .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                            .build();
+
+    return new InMemoryUserDetailsManager(employee, manager, admin);
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+        (configurer
+         -> configurer.requestMatchers(HttpMethod.GET, "/api/employees")
+                .hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**")
+                .hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees")
+                .hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees")
+                .hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees")
+                .hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/basic/all")
+                .hasRole("EMPLOYEE")
+                .requestMatchers("/error")
+                .permitAll()
+                .anyRequest()
+                .permitAll()
+
+             ));
+    http.httpBasic(Customizer.withDefaults());
+
+    http.csrf(csrf -> csrf.disable());
+    // http.cors(cors -> cors.disable());
+    return http.build();
+  }
+
+
+}
--- cors-test-java/src/main/java/com/harald/corstestjava/controller/BasicController.java
+++ cors-test-java/src/main/java/com/harald/corstestjava/controller/BasicController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5500")
+// @CrossOrigin(origins = "http://localhost:5500")
 @RequestMapping("/basic")
-
 public class BasicController {
 
 
