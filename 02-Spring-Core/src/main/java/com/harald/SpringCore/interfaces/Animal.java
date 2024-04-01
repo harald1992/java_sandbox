a308998e81109cf5ce523e867e727cb0b23d8c5e
harald.hiemstra <haraldhiemstra@Haralds-MacBook-Pro.local>
2024-04-01 14:11:29 +0200

    cleanup start projects

--- 02-Spring-Core/src/main/java/com/harald/SpringCore/interfaces/Animal.java
+++
@@ -1,5 +0,0 @@
-package com.harald.SpringCore.interfaces;
-
-public interface Animal {
-String makeSound();
-}
--- 02-Spring-Core/src/main/java/com/harald/SpringCore/interfaces/Coach.java
+++
@@ -1,5 +0,0 @@
-package com.harald.SpringCore.interfaces;
-
-public interface Coach {
-String getDailyWorkout();
-}
