38566fc62e5ecc07eef729bf37eebb56ae30b0d1
Harald1992 <harald_1992@hotmail.com>
2024-10-28 13:17:49 +0100

    auditable creationtimestamp comment

--- onsen-auth-service/src/main/java/com/harald/onsenauthservice/entity/Auditable.java
+++ onsen-auth-service/src/main/java/com/harald/onsenauthservice/entity/Auditable.java
@@ -1,16 +1,19 @@
 package com.harald.onsenauthservice.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Data;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+// CAN NOW USE @CreationTimestamp and @UpdateTimestamp pricate Instant creationTime instead of this
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class) // When an entity lifecycle event occurs, such as before persisting, updating, or deleting an entity, the corresponding listener methods in the specified listener classes are invoked.
